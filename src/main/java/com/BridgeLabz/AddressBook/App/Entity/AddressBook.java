b333975e2719b2c4a3fa9fcfcd97d6456f63e578
puneet0410 <puneetp2200@gmail.com>
2025-03-06 23:06:11 +0530

    UC_2

--- src/main/java/com/BridgeLabz/AddressBook/App/Entity/AddressBook.java
+++ src/main/java/com/BridgeLabz/AddressBook/App/Entity/AddressBook.java
@@ -11,7 +11,5 @@ public class AddressBook {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
-    private String phone;
-    private String address;
+
 }
